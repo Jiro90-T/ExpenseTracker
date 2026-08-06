45bf613edd85a3149e0032fa5f6cd4cc014488fe
MiniMax-M3 <291324429+Jiro90-T@users.noreply.github.com>
2026-08-06 09:25:48 +0800

    chore: bump version to 0.21.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "0.21.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
